743bc6afe0b675d7a4216b461a8b67fab9de4bfd
arya <arya.lavasani@gmail.com>
2025-01-20 11:48:03 +0100

    [feat] finalize the project

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -7,44 +7,70 @@ import { faHandPointUp,faXmark,faHandPointDown } from '@fortawesome/free-solid-s
 function TodoList() {
 
     const [tasks, setTasks] = useState([])
+
+
+    function toggleCheckList(index) {
+        const updatedTasks = tasks.map((task, i) =>
+            i === index ? { ...task, checked: !task.checked } : task
+          );
+          setTasks(updatedTasks);
+    }
     function add(e) {
         if(e.key !== 'Enter') return;
 
         e.preventDefault();
         const task = document.querySelector('input[name=task]').value;
         document.querySelector('input[name=task]').value = '';
-        setTasks(prevTask => [...prevTask, task]);
+        const newTask = {task: task, checked:false}
+        setTasks(prevTask => [...prevTask, newTask]);
+        
     }
 
-    function remove(e) {
-
-
-        
-        const TaskElement = e.target.closest("li");
-        const taskText = TaskElement.querySelector(".task-text").textContent;
-        e.preventDefault();        
-        let newTasks = tasks.filter(task => task !== taskText);
+    function remove(removedIndex) {
+        // const TaskElement = e.target.closest("li");
+        // const taskText = TaskElement.querySelector(".task-text").textContent;
+        let newTasks = tasks.filter((_,index) => index !== removedIndex);
         setTasks(newTasks);
     }
 
-    function moveTaskUp(e) {
-        const TaskElement = e.target.closest("li");
-        const taskText = TaskElement.querySelector(".task-text").textContent;
-        const taskIndex = tasks.indexOf(taskText);
+    function moveTaskUp(taskIndex) {
         if (taskIndex === 0) return;
         const newTasks = [...tasks];
         [newTasks[taskIndex], newTasks[taskIndex - 1]] = [newTasks[taskIndex - 1], newTasks[taskIndex]];
         setTasks(newTasks);
     }
 
-    function moveTaskDown(e) {
-        const TaskElement = e.target.closest("li");
-        const taskText = TaskElement.querySelector(".task-text").textContent;
-        const taskIndex = tasks.indexOf(taskText);
-        if (taskIndex === tasks.length - 1) return;
+    function moveTaskDown(index) {
+        if (index === tasks.length - 1) return;
         const newTasks = [...tasks];
-        [newTasks[taskIndex], newTasks[taskIndex + 1]] = [newTasks[taskIndex + 1], newTasks[taskIndex]];
+        [newTasks[index], newTasks[index + 1]] = [newTasks[index + 1], newTasks[index]];
         setTasks(newTasks);
+    }
+
+
+    function renderTasks() {
+
+        return tasks.map((task,index) => {
+            return (
+                <li className={`task ${task.checked ? "checked" : ""}`} key={index} onClick={() => toggleCheckList(index)}>
+                        <div className='task-info'>
+                            <input type='checkbox' className='task-checkbox' checked={task.checked} readOnly />
+                            <span className='task-text'>{task.task}</span>
+                        </div>
+                        <div className='task-buttons'>
+                            <button className='remove-task' onClick={() => remove(index)}>
+                                <FontAwesomeIcon icon={faXmark} />
+                            </button>
+                            <button className='move-task-up' onClick={() =>moveTaskUp(index)}>
+                                <FontAwesomeIcon icon={faHandPointUp} />
+                            </button>
+                            <button className='move-task-down' onClick={() => moveTaskDown(index)}>
+                                <FontAwesomeIcon icon={faHandPointDown} />
+                            </button>
+                        </div>
+                </li>
+            )
+        })
     }
 
 
@@ -53,25 +79,7 @@ function TodoList() {
         <>
             <h1>Todo List</h1>
             <ul className='tasks'>
-                {tasks.map((task, index) => (
-                    <li className='task' key={index}>
-                
-                        <span className='task-text'>
-                        {task}
-                        </span>
-                        <div className='task-buttons'>
-                            <button className='remove-task' onClick={remove}>
-                            <FontAwesomeIcon icon={faXmark} />
-                            </button>
-                            <button className='move-task-up' onClick={moveTaskUp}>
-                                <FontAwesomeIcon icon={faHandPointUp} />
-                            </button>
-                            <button className='move-task-down' onClick={moveTaskDown}>
-                                <FontAwesomeIcon icon={faHandPointDown} />
-                            </button>
-                        </div>
-                    </li>
-                ))}
+                {renderTasks()}
             </ul>
 
             <input type="text" id='task-input' onKeyDown={add} name="task" />
